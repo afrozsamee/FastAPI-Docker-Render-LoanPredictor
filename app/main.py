bd9336d04ed7262d1e9183b6b0f359171a3d34fe
afrozsamee <afrozsamee22@gmail.com>
2025-11-28 06:17:56 +0000

    Fix CORS settings

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.model.model import predict_pipeline, __version__ as model_version
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Loan Approval API")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ----------------------------
 # Input schema (RAW features)
